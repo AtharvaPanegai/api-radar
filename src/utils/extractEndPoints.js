70800d8a7acb645028c3dce35b48dedfb47e2fa7
AtharvaPanegai <panegai.atharv@gmail.com>
2024-07-27 18:18:43 +0530

    Adding helper function of helper function which cleans the ouput of apis

--- src/utils/extractEndPoints.js
+++ src/utils/extractEndPoints.js
@@ -1,3 +1,13 @@
+// this is helper function of helper function which will return us clean output of
+const _extractBasePath = (regexSource) => {
+    const cleanedPath = regexSource
+      .replace(/^\^\\/, '')
+      .replace(/\\\/?$/, '')
+      .replace(/(?=\(.*\))/, '')
+      .replace(/\\\//g, '/');
+  
+    return cleanedPath.replace(/\?\(\?=\/\|\$\)/g, '').replace(/\/+$/, '');
+};
 
 // This function grabs the routes from app route stacks and push them into endpoint array
 export const extractEndpoints = (stack, basePath = '', endpoints = []) => {
@@ -9,10 +19,11 @@ export const extractEndpoints = (stack, basePath = '', endpoints = []) => {
                 endpoints.push({ method, path });
             });
         } else if (middleware.name === 'router' && middleware.handle.stack) { // Router middleware
-            const nestedBasePath = basePath + extractBasePath(middleware.regexp.source);
+            const nestedBasePath = basePath + _extractBasePath(middleware.regexp.source);
             extractEndpoints(middleware.handle.stack, nestedBasePath);
         }
     });
     return endpoints;
 };
+
 
