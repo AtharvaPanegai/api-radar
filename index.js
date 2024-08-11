c1332da3ae61e22fc334a1752b092caca760e6a3
AtharvaPanegai <panegai.atharv@gmail.com>
2024-08-11 12:37:39 +0530

    Adding more functionality in the package to collect more information from the service

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const collectApiEndpoints = require("./src/controllers/collectApiEndpoints");
+const apiCollector = require("./src/controllers/apiCollector");
 
 module.exports = {
+  apiCollector,
   collectApiEndpoints
 }
---
+++ src/controllers/apiCollector.js
@@ -0,0 +1,40 @@
+const { default: axios } = require("axios");
+const collectApiEndpoints = require("./collectApiEndpoints");
+
+const apiCollector = (app) => {
+  app.use((req, res, next) => {
+    if (!req.app.locals.apiEndpoints) {
+      req.app.locals.apiEndpoints = collectApiEndpoints(app);
+    }
+    next();
+  });
+
+  app.use((req, res, next) => {
+    const start = process.hrtime();
+
+    const logData = {
+      method: req.method,
+      path: req.originalUrl,
+      headers: req.headers,
+      params: req.params,
+    };
+
+    res.on('finish', () => {
+      const duration = process.hrtime(start);
+      const durationInMs = duration[0] * 1000 + duration[1] / 1e6;
+
+      logData.statusCode = res.statusCode;
+      logData.responseTime = `${durationInMs.toFixed(3)} ms`;
+
+      console.log(logData);
+
+      // Send log data to radar-api
+      // axios.post("",logData); //fire and forget
+    });
+
+    // return logData;
+    next();
+  });
+};
+
+module.exports = apiCollector;
